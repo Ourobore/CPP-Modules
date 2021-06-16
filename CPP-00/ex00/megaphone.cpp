62913a6ae224872304fe283f8949b4ff62127130
Ourobore <chaprenet.loic@gmail.com>
2021-06-16 14:28:42 +0200

    CPP00 ex00 exercice and Makefile done

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/16 12:50:48 by lchapren          #+#    #+#             */
+/*   Updated: 2021/06/16 13:34:33 by lchapren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	size_t	j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (j < strlen(argv[i]))
+		{
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
