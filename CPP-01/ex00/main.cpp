8d1cdb239a9e6fb41916a958996092aec1af9973
Ourobore <chaprenet.loic@gmail.com>
2021-06-23 16:28:18 +0200

    Change confusion between stack and heap

--- CPP-01/ex00/main.cpp
+++ CPP-01/ex00/main.cpp
@@ -12,40 +12,40 @@
 
 #include "header.hpp"
 
-void	ponyOnTheHeap(void)
-{
-	Pony	pony_heap("Jacob", "Shetland", 120, 200);
-	
-	pony_heap.printPony();
-}
-
 void	ponyOnTheStack(void)
 {
-	Pony	*pony_stack = new Pony("Paquerette", "Connemara", 160, 320);
+	Pony	pony_stack("Jacob", "Shetland", 120, 200);
 	
-	pony_stack->printPony();
+	pony_stack.printPony();
+}
 
-	delete pony_stack;
+void	ponyOnTheHeap(void)
+{
+	Pony	*pony_heap = new Pony("Paquerette", "Connemara", 160, 320);
+	
+	pony_heap->printPony();
+
+	delete pony_heap;
 }
 
 int	main(void)
 {
-	Pony	pony_heap_main("Crimson", "Falabella", 60, 35);
-	Pony	*pony_stack_main = new Pony("Isabella", "Welsh", 140, 280);
+	Pony	pony_stack_main("Crimson", "Falabella", 60, 35);
+	Pony	*pony_heap_main = new Pony("Isabella", "Welsh", 140, 280);
 
 	std::cout << std::endl;
-	std::cout << "### Heap (in main) ###" << std::endl;
-	pony_heap_main.printPony();
-	std::cout << std::endl;
 	std::cout << "### Stack (in main) ###" << std::endl;
-	pony_stack_main->printPony();
+	pony_stack_main.printPony();
 	std::cout << std::endl;
-	std::cout << "### Heap (in function) ###" << std::endl;
-	ponyOnTheHeap();
+	std::cout << "### Heap (in main) ###" << std::endl;
+	pony_heap_main->printPony();
 	std::cout << std::endl;
 	std::cout << "### Stack (in function) ###" << std::endl;
 	ponyOnTheStack();
 	std::cout << std::endl;
-	delete pony_stack_main;
+	std::cout << "### Heap (in function) ###" << std::endl;
+	ponyOnTheHeap();
+	std::cout << std::endl;
+	delete pony_heap_main;
 	return (0);
 }
