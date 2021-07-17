cb7fde3744ad714760acc1fe93592108b7f0d9b6
Ourobore <chaprenet.loic@gmail.com>
2021-07-17 15:49:50 +0200

    Added some Form member functions

---
+++ CPP-05/ex01/Form.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/17 13:21:07 by user42            #+#    #+#             */
+/*   Updated: 2021/07/17 15:48:44 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bureau.hpp"
+
+Form::Form(void) : _name("Unknow"), _signGrade(150), _execGrade(150), _signed(false)
+{
+}
+
+Form::Form(std::string const &name, unsigned int const signGrade, unsigned int const execGrade) \
+: _name(name), _signGrade(signGrade), _execGrade(execGrade), _signed(false)
+{
+	if (_signGrade < 1 || _execGrade < 1)
+		throw Form::GradeTooHighException();
+	if (_signGrade > 150 || _execGrade > 150)
+		throw Form::GradeTooLowException();
+}
+
+Form::Form(Form const &rhs) : \
+_name(rhs.getName()), _signGrade(rhs.getSignGrade()), _execGrade(rhs.getExecGrade()), _signed(false)
+{
+}
+
+Form& Form::operator=(Form const &rhs)
+{
+	(void)rhs;
+	return (*this);
+}
+
+Form::~Form(void)
+{
+}
+
+std::string const Form::getName(void) const
+{
+	return (_name);
+}
+
+unsigned int Form::getSignGrade(void) const
+{
+	return (_signGrade);
+}
+
+unsigned int Form::getExecGrade(void) const
+{
+	return (_execGrade);
+}
+
+bool Form::getSignature(void) const
+{
+	return (_signed);
+}
+
+std::ostream&	operator<<(std::ostream &o, Form const &rhs)
+{
+	o << "Form: <" << rhs.getName() << ">, Sign grade <" << rhs.getSignGrade() \
+	<< "> and Exec grade <" << rhs.getExecGrade() << ">. Signed: " << rhs.getSignature();
+	return (o);
+}
---
+++ CPP-05/ex01/main.cpp
@@ -0,0 +1,134 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/17 10:53:39 by user42            #+#    #+#             */
+/*   Updated: 2021/07/17 15:49:22 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bureau.hpp"
+
+int	main(void)
+{
+/*
+	std::cout << "===== Ex00 =====" << std::endl;
+	std::cout << "### Constructors ###" << std::endl;
+	Bureaucrat a;
+	std::cout << a << std::endl;
+	
+	// b will not be accessible outside off this try()
+	try
+	{
+		Bureaucrat b("Ace", 1);
+		std::cout << b << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+
+	// Constructor GradeTooHighException
+	std::cout << "### Constructor GradeTooHighException (0) ###" << std::endl;
+	try
+	{
+		Bureaucrat b("Ace", 0);
+		std::cout << b << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	
+
+	// Constructor GradeTooLowException
+	std::cout << "### Constructor GradeTooLowException (151) ###" << std::endl;
+	try
+	{
+		Bureaucrat f("Ace", 151);
+		std::cout << f << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	
+	// Increment GradeTooHighException
+	std::cout << "### Increment GradeTooHighException (1++) ###" << std::endl;
+	Bureaucrat c("Beta", 2);
+	std::cout << "Before: " << c << std::endl;
+	try
+	{
+		c.incGrade();
+		std::cout << "Succes: " << c << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Failure: " << e.what() << std::endl;
+	}
+	std::cout << "First inc: " << c << std::endl;
+	try
+	{
+		c.incGrade();
+		std::cout << "Succes: " << c << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Failure: " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+
+	// Decrement GradeTooLowException
+	std::cout << "### Increment GradeTooLowException (150--) ###" << std::endl;
+	Bureaucrat g("Omega", 149);
+	std::cout << c << std::endl;
+	try
+	{
+		g.decGrade();
+		std::cout << "Succes: " << g << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Failure: " << e.what() << std::endl;
+	}
+	std::cout << "First dec: " << g << std::endl;
+	try
+	{
+		g.incGrade();
+		std::cout << "Succes: " << c << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Failure: " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	
+	//Assignation
+	std::cout << "### Assignation (c = a) ###" << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "c before: " << c << std::endl;
+	c = a;
+	std::cout << "c after: " << c << std::endl;
+	std::cout << std::endl;
+	
+	//Copy constructor
+	std::cout << "### Copy constructor (from c) ###" << std::endl;
+	std::cout << "c: " << c << std::endl;
+	Bureaucrat d(c);
+	std::cout << "d: " << d << std::endl;
+	std::cout << std::endl;
+*/
+
+	std::cout << "===== Ex01 =====" << std::endl;
+	Form a;
+	Form b("Plop", 150, 100);
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+}
