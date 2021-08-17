dd08938ec10df2e6ec2ebca8e9f7177deac6a974
Ourobore <chaprenet.loic@gmail.com>
2021-08-17 15:24:14 +0200

    Ex02 almost done

--- CPP-07/ex02/Array.hpp
+++ CPP-07/ex02/Array.hpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/17 12:17:22 by lchapren          #+#    #+#             */
-/*   Updated: 2021/08/17 13:42:39 by lchapren         ###   ########.fr       */
+/*   Created: 2021/08/17 14:56:55 by lchapren          #+#    #+#             */
+/*   Updated: 2021/08/17 15:22:09 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,69 @@ template <typename T>
 class Array
 {
 	private:
-		T*	_value;
+		T* _value;
+		unsigned int _size;
 
 	public:
-		Array(void);
-		Array(unsigned int n);
-		Array(Array const &rhs);
-		Array& operator=(Array const &rhs);
-		~Array(void);
+		Array(void) : _value(NULL), _size(0) {}
 
-		T* getValue(void);
-		void setValue(T value);
+		Array(unsigned int n) : _size(n)
+		{
+			_value = new T[n];
+			for (unsigned int i = 0; i < n; i++)
+				_value[i] = 0;
+		}
+		
+		Array(Array const &rhs)
+		{
+			_size = rhs.getSize();
+			_value = new T[_size];
+			for (unsigned int i = 0; i < _size; i++)
+				_value[i] = rhs.getValue()[i];
+		}
+
+		Array& operator=(Array const &rhs)
+		{
+			if (this != &rhs)
+			{
+				if (_size)
+					delete[] _value;
+				_size = rhs.getSize();
+				_value = new T[_size];
+				for (unsigned int i = 0; i < _size; i++)
+					_value[i] = rhs.getValue()[i];
+			}
+			return (*this);
+		}
+		
+		T& operator[](int index)
+		{
+			if (index < 0 || index >= static_cast<int>(_size))
+				throw std::exception();
+			else
+				return (_value[index]);
+		}
+
+		~Array(void)
+		{
+			if (_size)
+				delete[] _value;
+		}
+
+		unsigned int getSize(void) const
+		{
+			return (_size);
+		}
+
+		T* getValue(void) const
+		{
+			return (_value);
+		}
+
+		void setValue(T value)
+		{
+			_value = value;
+		}
 };
 
 #endif
--- CPP-07/ex02/main.cpp
+++ CPP-07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 12:17:44 by lchapren          #+#    #+#             */
-/*   Updated: 2021/08/17 13:41:56 by lchapren         ###   ########.fr       */
+/*   Updated: 2021/08/17 15:21:28 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,37 @@
 
 int main(void)
 {
-	int *a = new int[10];
-	for (int i = 0; i < 10 ; i++)
-		std::cout << a[i] << std::endl;
+	//int *a = new int[10];
+	//for (int i = 0; i < 10 ; i++)
+	//	std::cout << a[i] << std::endl;
+
+	//Array<int> q;
+	Array<int> z(5);
+	
+	for (unsigned int i = 0; i < z.getSize(); i++)
+		std::cout << z.getValue()[i] << std::endl;
+	
+	for (unsigned int i = 0; i < z.getSize(); i++)
+		z.getValue()[i] = i;
+		
+	std::cout << std::endl;
+	for (unsigned int i = 0; i < z.getSize(); i++)
+		std::cout << z.getValue()[i] << std::endl;
+	
+	Array<int> a;
+	std::cout << std::endl;
+	for (unsigned int i = 0; i < a.getSize(); i++)
+		std::cout << a.getValue()[i] << std::endl;
+
+	a = z;
+	std::cout << std::endl;
+	for (unsigned int i = 0; i < a.getSize(); i++)
+		std::cout << a.getValue()[i] << std::endl;
+
+	Array<int> n(a);
+	std::cout << std::endl;
+	for (unsigned int i = 0; i < n.getSize(); i++)
+		std::cout << n.getValue()[i] << std::endl;
+
+	std::cout << n[-1] << std::endl;
 }
